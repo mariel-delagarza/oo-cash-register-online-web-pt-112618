668374ad0f8d85d02703e1d10f219a247fb27733
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 01:43:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,9 @@
 class CashRegister
-  attr_accessor :items, 
+  attr_accessor :items, :discount, :total, :last_transaction 
+  
+  def initialize(discount=0)
+    @total = 0 
+  end 
   
   def total 
   end 
