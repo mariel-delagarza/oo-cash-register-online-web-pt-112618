451a482d74b967d7e04b040aedd15eaf3e8084e1
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 01:48:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,8 @@ class CashRegister
   def add_item(title, price, quantity=1)
     self.total += price * quantity 
     quantity.times do 
-      
+      items << title 
+    
   end 
 
   def apply_discount
