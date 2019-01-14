53f1faacf00f95869739eb9eb8ebb9046997b194
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 01:56:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
 
   def apply_discount
     if discount != 0 
-    discount_total = @total * @discount * 100
+    self.total = @total * @discount * 100
     puts "Success! Your total is now #{discount_total}!"
   end 
   
